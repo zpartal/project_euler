4a32b716fc078ed195e1360796f3e64a5980aa30
Zachary Partal <zpartal@gmail.com>
2013-03-24 16:40:10 -0500

    finished 6

--- euler_6.cpp
+++ euler_6.cpp
@@ -17,6 +17,15 @@ using namespace std;
 int main() {
 	int sum_squares = 0;
 	int square_sums = 0;
-
+	
+	int sum = 0;
+	for (int i = 1; i <= 100; i++) {
+		sum_squares += i*i;
+		sum += i;
+	}
+	square_sums = sum*sum;
+	int output = square_sums - sum_squares;
+	cout << output << endl;
+	
 	return 0;
 }
