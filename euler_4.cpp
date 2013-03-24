5602e4482aabee42aa8dde995ef072f83a7fc665
Zachary Partal <zpartal@gmail.com>
2013-03-24 17:22:44 -0500

    finished 4 starting 8

--- euler_4.cpp
+++ euler_4.cpp
@@ -1,5 +1,5 @@
 /*
-Project Euler Problem 7
+Project Euler Problem 4
 
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91*99.
 
@@ -13,10 +13,30 @@ Zachary Partal
 
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
+bool isPalindrome(int num) {
+	stringstream ss; 
+	ss << num;
+	string num_str = ss.str();
+	if (num_str == string(num_str.rbegin(), num_str.rend())) return true;
+	else return false;	
+}
+
 int main() {
+	int largest = 0;
+	for (int i = 100; i < 1000; i++) {
+		for (int j = 100; j < 1000; j++) {
+			if (isPalindrome(i*j)) {
+				cout << "i*j: " << i*j << " i: " << i << " j: " << j << endl;
+				if ((i*j) > largest) largest = i*j;
+			}
+		}
+	}
+	cout << largest;
 
 	return 0;
 }
